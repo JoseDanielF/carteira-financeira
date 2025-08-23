476f30002006fde23469a0735e7e8de1bcfe76ff
Daniel Duarte <danielduartefilho.df@gmail.com>
2025-08-22 21:32:53 -0300

    adicionado rotas de logout e transações na api

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,20 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user()->load('wallet');
+    });
+
+    Route::post('/logout', [AuthController::class, 'logout']);
+
     Route::post('/deposit', [TransactionController::class, 'deposit']);
     Route::post('/transfer', [TransactionController::class, 'transfer']);
     Route::post('/transactions/{transaction}/reverse', [TransactionController::class, 'reverse']);
+    Route::get('/transactions', [TransactionController::class, 'index']);
+    Route::get('/transactions/{transaction}', [TransactionController::class, 'show']);
+    Route::delete('/transactions/{transaction}', [TransactionController::class, 'destroy']);
+    Route::get('/wallet', function (Request $request) {
+        return $request->user()->wallet;
+    });
+    Route::get('/wallet/transactions', [TransactionController::class, 'walletTransactions']);
 });
